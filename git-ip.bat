83657c2278414ebfd49b13dafbe4b2eb3a087e36
anjanposa6 <168364487+anjanposa6@users.noreply.github.com>
2026-01-20 14:57:47 +0530

    Update git-ip.bat

--- git-ip.bat
+++ git-ip.bat
@@ -1,3 +1,14 @@
-ip=$(hostname -I | awk '{print $1}')
- 
-echo "Local IP Address: $ip"
+@echo off
+REM Get the hostname
+set HOSTNAME=%COMPUTERNAME%
+
+REM Get the IP address (first one found)
+for /f "tokens=2 delims=:" %%a in ('ipconfig ^| findstr /c:"IPv4 Address"') do (
+    set IP=%%a
+)
+
+REM Trim spaces
+set IP=%IP: =%
+
+echo Hostname: %HOSTNAME%
+echo IP Address: %IP%
